e48bc91d8392f9c4384abe24454c229457f0fbe5
Sam Wu <22262939+samjwu@users.noreply.github.com>
2024-10-14 21:47:08 -0600

    Add subfolder for header only lib

---
+++ Ex9/calculator_headeronly/include/calculator_headeronly.h
@@ -0,0 +1,12 @@
+#ifndef CALCULATOR_HEADERONLY_H_
+#define CALCULATOR_HEADERONLY_H_
+
+class CalculatorHeaderOnly
+{
+public:
+    int Add(int a, int b) {
+        return a + b;
+    }
+};
+
+#endif
